387b787fe53a2db596613f04990a8bbe9a962851
Alexander Lezama <falcone_21@hotmail.com>
2016-08-28 13:50:04 -0500

    Creacion de Model y Repository

---
+++ WebPractica2.Model/InvoiceLine.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebPractica2.Model
+{
+
+
+    [Table("InvoiceLine")]
+    public partial class InvoiceLine
+    {
+        public int InvoiceLineId { get; set; }
+
+        public int InvoiceId { get; set; }
+
+        public int TrackId { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public virtual Invoice Invoice { get; set; }
+
+        public virtual Track Track { get; set; }
+    }
+}
